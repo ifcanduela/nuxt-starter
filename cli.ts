67b1bdad8864ecdc39766763f81ac9b055d78423
Igor Canduela <i.canduela@terbergbusinesslease.nl>
2024-02-19 15:07:14 +0100

    Inline subcommand definitions

--- cli.ts
+++ cli.ts
@@ -7,45 +7,41 @@ import { defineCommand, runMain } from "citty"
 import consola from "consola"
 import TestCommand from "./commands/TestCommand.ts"
 
-const test = defineCommand({
-	meta: {
-		name: "test",
-		description: "This is a sub-command",
-	},
-	run: TestCommand,
-})
-
-const print = defineCommand({
-	meta: {
-		name: "print",
-	},
-	args: {
-		name: {
-			type: "positional",
-			description: "Your name",
-			required: true,
-		},
-		friendly: {
-			type: "boolean",
-			description: "Use friendly greeting",
-		},
-	},
-	run(payload) {
-		consola.warn("The print command is not yet implemented")
-		consola.warn("But here are the arguments you passed:")
-		console.log(payload.rawArgs)
-	},
-})
-
 const main = defineCommand({
 	meta: {
-		name: "hello",
+		name: "cli",
 		version: "1.0.0",
 		description: "My Awesome CLI App",
 	},
 	subCommands: {
-		print: print,
-		test: test,
+		print: defineCommand({
+			meta: {
+				name: "print",
+			},
+			args: {
+				name: {
+					type: "positional",
+					description: "Your name",
+					required: true,
+				},
+				friendly: {
+					type: "boolean",
+					description: "Use friendly greeting",
+				},
+			},
+			run(payload) {
+				consola.warn("The print command is not yet implemented")
+				consola.warn("But here are the arguments you passed:")
+				console.log(payload.rawArgs)
+			},
+		}),
+		test: defineCommand({
+			meta: {
+				name: "test",
+				description: "This is a sub-command",
+			},
+			run: TestCommand,
+		}),
 	},
 })
 
